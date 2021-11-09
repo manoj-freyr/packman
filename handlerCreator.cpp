77cdce3bea5961398da844308ddcb2a50a7011de
Manoj S K <Manoj.SK@amd.com>
2021-11-09 07:04:33 +0000

    dummy

--- handlerCreator.cpp
+++
@@ -1,8 +0,0 @@
-#include "handlerCreator.h"
-std::unique_ptr<PackageHandler> handlerCreator::getPackageHandler(){
-	auto osName = getOS();
-	if(osName.find("ubuntu") != std::string::npos)
-		return std::unique_ptr<PackageHandler>(new PackageHandlerDeb{});
-	else
-		return nullptr;
-}
