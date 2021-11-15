da94f25eba94f32d9cd11e0dd3254fe5b4fa027c
Manoj S K <Manoj.SK@amd.com>
2021-11-15 07:03:59 -0800

    making path as CL argument

--- action.cpp
+++ action.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include "handlerCreator.h"
 
-int main(){
+
+void print_usage(){
+  std::cout << "Tool to validate packages installed " << std::endl;
+  std::cout << "Usage as below: " << std::endl;
+  std::cout << "./packman <path_to_package_list_file>" << std::endl;
+  std::cout << "example: " << std::endl;
+  std::cout << "\t" << "./packman /home/amd/package-list.txt" << std::endl;
+}
+
+bool isFileValid(const std::string& filePath){
+  std::ifstream f{filePath.c_str()};
+	return f.good();
+}
+
+int main(int Argc, char**Argv){
+	if(Argc != 2){
+    print_usage();
+    return -1;
+  }
 	handlerCreator creator;
 	auto hander = creator.getPackageHandler();
 	if(!hander){
 		std::cout << "Failed to handler " << std::endl;
 		return -1;
 	}
-	hander->setManifest("/home/amd/freyr/exper/packman/packman/package-list.txt");
+  std::string pathname{Argv[1]};
+  if(!isFileValid(pathname)){
+    std::cout << " Invalid path for package list file " << std::endl;
+    return -1;
+  } 
+	//hander->setManifest("/home/amd/freyr/exper/packman/packman/package-list.txt");
+	hander->setManifest(pathname);
 	hander->parseManifest();
 	hander->validatePackages();	
 	return 0;	
